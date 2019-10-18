6bcd0be75df0a5e75871a020b9fc16bbbb019205
bart-tek <barthelemy.tek@imt-atlantique.net>
2019-10-18 10:02:40 +0200

    Commentaires

--- internal/confcaptorstruct/confcaptrostruct.go
+++ internal/confcaptorstruct/confcaptrostruct.go
@@ -7,10 +7,12 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// ConfCaptorStruct Structure récupérant la configuration des capteurs depuis un ficheir de de configuration .yaml
 type ConfCaptorStruct struct {
 	ListeAeroport []string `yaml:"airports"`
 }
 
+// GetConf Méthode qui lit le fichier de configuration, le parse et peuple la structure avec la configuration récupérée
 func (c *ConfCaptorStruct) GetConf() *ConfCaptorStruct {
 	yamlFile, err := ioutil.ReadFile("internal/conf/confCaptor.yaml")
 	if err != nil {
--- internal/enumnature/enum_nature.go
+++ internal/enumnature/enum_nature.go
@@ -1,7 +1,12 @@
 package enumnature
 
 const (
+	// TEMP Temperature
 	TEMP = "Temperature"
+
+	// PRES Pression
 	PRES = "Atmospheric pressure"
+
+	// WIND Wind
 	WIND = "Wind speed"
 )

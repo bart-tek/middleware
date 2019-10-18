8c3b75f80b2b69065e3cade0efb83750550befb0
axel COUDRAY <axel.hpm.coudray@gmail.com>
2019-10-18 11:14:59 +0200

    wip ui

---
+++ front/samples.js
@@ -0,0 +1,51 @@
+(function(global) {
+
+	var Samples = global.Samples || (global.Samples = {});
+
+	Samples.items = [{
+		title: 'Airports',
+		items: [{
+			title: 'ORY',
+			path: 'chart.html?airport_id=ORY&title=ORLY'
+		}, {
+			title: 'NCE',
+			path: ''
+		}, {
+			title: 'LYS',
+			path: ''
+		}, {
+			title: 'TLS',
+			path: ''
+		}, {
+			title: 'MRS',
+			path: ''
+		}, {
+			title: 'BSL',
+			path: ''
+		}, {
+			title: 'BOD',
+			path: ''
+		}, {
+			title: 'NTE',
+			path: ''
+		}, {
+			title: 'BVA',
+			path: ''
+		}, {
+			title: 'RUN',
+			path: ''
+		}, {
+			title: 'PTP',
+			path: ''
+		}, {
+			title: 'LIL',
+			path: ''
+		}, {
+			title: 'FDF',
+			path: ''
+		}, {
+			title: 'MPL',
+			path: ''
+		}]
+	}];
+}(this));

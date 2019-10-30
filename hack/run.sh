127baa66ad0b8a34ea3d2308d6ce1e5e0c4da53b
Evrard-Nil <evrardnild@gmail.com>
2019-10-30 10:19:19 +0100

    Build and docker

--- hack/run.sh
+++ hack/run.sh
@@ -1,3 +1,10 @@
 #! bash
-docker run --name redis_middleware_dev --network=host -d redis
-# Start all main.go
+# build all main.go
+
+go build -o ./bin/redis_sub   ./cmd/redis_sub/
+go build -o ./bin/csv_sub     ./cmd/csv_sub/
+go build -o ./bin/temp_captor ./cmd/captors/temp_captor/
+go build -o ./bin/wind_captor ./cmd/captors/wind_captor/
+go build -o ./bin/pres_captor ./cmd/captors/pressure_captor/
+go build -o ./bin/json_serv   ./cmd/json_serv/
+
